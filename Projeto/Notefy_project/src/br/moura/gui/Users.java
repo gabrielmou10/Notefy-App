24b77e0e94033191646a44b5d2236a19f344911c
Gabrielmou <gabrielmoura98@gmail.com>
2018-09-04 08:57:21 -0300

    PROJ1

---
+++ Projeto/Notefy_project/src/br/moura/gui/Users.java
@@ -0,0 +1,22 @@
+package br.moura.gui;
+
+public class Users {
+	
+	private Integer id;
+	private String name;
+	private String user;
+	private String password;
+	
+	public Integer getId() {return this.id;}
+	public void setId(Integer id) {this.id = id;}
+	
+	public String getNome() {return this.name;}
+	public void setNome(String name) {this.name = name;}
+	
+	public String getUser() {return this.user;}
+	public void setUser(String user) {this.user = user;}
+	
+	public String getPassword() {return this.password;}
+	public void setPassword(String password) {this.password = password;}
+
+}
